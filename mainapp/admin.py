f5b1c90eca5aaf7c26fa8ceb74243b79acb327ec
azam <gausuleazamali@yopmail.com>
2023-06-29 00:18:56 +0530

    Update: updated school and student admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -5,10 +5,16 @@ from mainapp.models import School, Student
 
 class SchoolAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    list_display_links = ("id", "name")
+    list_per_page = 25
+    list_filter = ("created_at", "updated_at")
 
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("name", "enrollment", "school")
+    list_display_links = ("id", "name")
+    list_per_page = 25
+    list_filter = ("created_at", "updated_at")
 
 
 admin.site.register(School, SchoolAdmin)
